86fa6972f46e7369aab8aebfbe5ee72887173f75
prince-ao <prince.addo@cix.csi.cuny.edu>
2024-01-17 11:52:05 -0500

    made player

---
+++ Assets/TheifController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheifController : MonoBehaviour {
+
+    public Rigidbody theifRigidbody;
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+}
