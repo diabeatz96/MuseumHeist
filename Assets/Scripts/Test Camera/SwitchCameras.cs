7fd85931b6b8f37ef6fbd47d60d58bd3254cfd28
Adam Kostandy <adamkostandy@gmail.com>
2024-01-18 09:03:24 -0500

    Added in Level1 Changes
    Level1 Changes now Set.

--- Assets/Scripts/Test Camera/SwitchCameras.cs
+++ Assets/Scripts/Test Camera/SwitchCameras.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using UnityEngine.InputSystem;
+using TMPro; // Import the TextMeshPro namespace
 
 public class SwitchCameras : MonoBehaviour
 {
     public CinemachineVirtualCamera[] cameras;
+    public Camera mainCamera; // The main Camera component
+    public Camera tvCamera; // The second Camera component that's a child of the main camera
+    public MeshRenderer tvScreen; // The MeshRenderer of the TV screen
+    public TextMeshProUGUI cameraNameText; // The TextMeshPro component
     private int currentCameraIndex;
+    private RenderTexture renderTexture;
 
     // Start is called before the first frame update
     void Start()
     {
         currentCameraIndex = 0;
+        renderTexture = new RenderTexture(1920, 1080, 16); // Create a new RenderTexture
+        tvScreen.material.mainTexture = renderTexture; // Set the RenderTexture as the main texture of the TV screen
+        tvCamera.targetTexture = renderTexture; // Set the second Camera to output to the RenderTexture
+
         for (int i = 0; i < cameras.Length; i++)
         {
             cameras[i].Priority = i == currentCameraIndex ? 1 : 0;
         }
+
+        // Display the name of the current camera
+        cameraNameText.text = cameras[currentCameraIndex].name;
     }
 
     // Update is called once per frame
@@ -39,5 +52,8 @@ public class SwitchCameras : MonoBehaviour
         if (currentCameraIndex < 0) currentCameraIndex = cameras.Length - 1;
         if (currentCameraIndex >= cameras.Length) currentCameraIndex = 0;
         cameras[currentCameraIndex].Priority = 1;
+
+        // Update the TextMeshPro text to display the name of the current camera
+        cameraNameText.text = cameras[currentCameraIndex].name;
     }
 }
