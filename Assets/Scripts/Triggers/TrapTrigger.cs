21a08e253113a9fc34b245a4603548ae2ea5f81a
Adam Kostandy <adamkostandy@gmail.com>
2024-01-22 14:58:09 -0500

    Refactor code to improve performance and readability

--- Assets/Scripts/Triggers/TrapTrigger.cs
+++ Assets/Scripts/Triggers/TrapTrigger.cs
@@ -1,25 +1,37 @@
 using UnityEngine;
+using Fusion;
 
-public class TrapTrigger : MonoBehaviour
+public class TrapTrigger : NetworkBehaviour
 {
     public GameController gameController; // Reference to the GameController
 
+    private float cooldownTime = 1.0f; // 1 second cooldown
+    private float nextTriggerTime = 0;
+
     void Start()
     {
-        // Get the GameController instance
+        // Get the GameController instance 
         gameController = GameObject.Find("GameManager").GetComponent<GameController>();   
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        Debug.Log("Trigger detected");
+        if (other.gameObject.CompareTag("Player") && Time.time > nextTriggerTime)
         {
+            nextTriggerTime = Time.time + cooldownTime;
             Debug.Log("Trap triggered");
-            // Move the player to the base spawner position
-            //other.gameObject.transform.position = gameController.baseSpawner.transform.position;
 
-            // Set hasBeenCaught to true and end the timer
-            gameController.hasBeenCaught = true;
-            gameController.EndTimerIsCaught();
+            // Check if we are on the server
+            if (Runner.IsServer)
+            {
+                // Move the player to the base spawner position
+                //other.gameObject.transform.position = gameController.baseSpawner.transform.position;
+
+                // Set hasBeenCaught to true and end the timer
+                gameController.hasBeenCaught = true;
+                gameController.EndTimerIsCaught();
+            }
         }
     }
 }
